dbf1b57a9f67979aa10bd72b2f29b8aa2de9656b
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-16 15:32:25 +0530

    Js-Tutorial

--- Js-Tutorial/script.js
+++ Js-Tutorial/script.js
@@ -1,7 +1,6 @@
 // variable
-var a = 10
-console.log(a);
-
+// var a = 10
+// console.log(a);
 
 
 // var a = 10
@@ -11,5 +10,312 @@ console.log(a);
 // console.log(a-b);
 // console.log(a/b);
 // console.log(a%5); 
+
+// alert('Hello')
+
+// var a = 10
+// a = 20
+// console.log(a);
+
+// var ans = confirm('are you an adult')
+// console.log(ans);
+
+// var a = confirm('Achha!!')
+// console.log(a);
+
+// var a = prompt('Enter your name! ')
+// console.log(a);
+
+// alert -> give message
+// confirm -> confirm krna
+// prompt -> user input
+
+
+// var naam = prompt('Enter user name ')
+// console.log('Username',naam);
+
+// JS Datatype
+// 1> Premetive -> single type of data
+
+        // - Number (1,2,3,20.5,6.66666)
+        // - String ('a','bharat')
+        // - Boolean (true, false)
+        // - undefined
+        // - null
+        // - NaN
+        // - Symbol
+
+// 2> Refrence 
+
+        // - Array
+        // - Object
+        // - Function
+
+// var a = 10
+// var b = 'bharat'
+// console.log(a+b);
+
+// conditiom
+// if(10>5){
+//     console.log('correct');
+    
+// }else{
+//     console.log('Not correct');
+    
+// }
+
+// var a = 10
+// var b =  20
+// if(a>b){
+//     console.log(a,'A is greater');
+    
+// }else{
+//     console.log(b,'B is greater than A');
+    
+// }
+
+// var a = 10
+// if(a>20){
+//     console.log('Hello!! ');
+    
+// }else{
+//     console.log('Not Hello!! ');
+    
+// }
+
+
+// var age = prompt('Enter your age! ')
+// if(age>18){
+//     console.log('you are adult! ');
+    
+// }else{
+//     console.log('you are not adult! ');
+    
+// }
+
+// Loops
+
+// var a = 0
+// while(a<5){
+//     console.log('Hello');
+//     a++
+// }
+
+// var a= 0
+// while(a<=5){
+//     console.log(a);
+//     a++
+    
+// }
+
+// for loop
+
+// for(var a=0; a<=5; a++){
+//     console.log(a);
+    
+// }
+
+// for(let a=0; a<=5; a++){
+//     console.log('bharat');
+    
+// }
+
+// comparison operators
+
+// a = 10
+// b = 10 // a = '10' sahi hai
+// if(a==b){
+//     console.log('Sahi hai ');
+    
+// }else{
+//     console.log('glat hai');
+    
+// }
+
+// Function
+
+// console.log('hello');
+// console.log('hello 2');
+// console.log('hello return ');
+// console.log('zinda hai ');
+
+// function walk(){
+//     console.log('Go');
+    
+// }
+// walk()
+
+// function hey(){
+//     console.log('Good morning ');
+    
+// }
+// hey()
+
+
+// function walk(){
+//     console.log('Chal to riya hun mere bhai ');
+    
+// }
+// function sing(){
+//     console.log('Main hoooon Giyaaaan');
+    
+// }
+// function dance(){
+//     console.log('💃💃💃💃');
+    
+// }
+// sing()
+// dance()
+// walk()
+
+
+// function greet(a){
+//     console.log('good morning! ',a);  
+// }
+// greet(10)
+// greet(20)
+// greet(30)
+
+// return function
+
+// function abc(){
+//     console.log('hello');
+    
+//     return 20   
+// }
+// var a = abc()
+// console.log(a);
+
+// var abc = function(){
+//     console.log('hello');
+    
+// }
+// abc()
+
+
+// arrow function
+
+// var abc = ()=>{
+// console.log('hello');
+
+// }
+// abc()
+
+// Array
+
+// var a = [10,20,30,40]
+// console.log(a);
+
+
+// var store = [10,'bharat',25.5,true,'hello']
+// console.log(store[1]);
+
+
+// add value in array
+// var arr = [10,20,30,40]
+// console.log(arr);
+
+// arr.push(50)
+// console.log(arr);
+// // remove last index in array
+// arr.pop()
+// console.log(arr);
+// console.log(arr.length);
+
+
+
+// forEach() method
+
+// var arr = [10,20,30,40,50,60]
+// arr.forEach(function(elm){
+//     console.log('hello',elm);
+    
+// })
+
+
+// var arr = [10,20,30,40,50]
+// arr.forEach(function(item){
+//     console.log('hey',item);
+    
+// })
+
+// () Function
+// {} Object
+// [] Array
+
+
+// Object
+// var obj = { 
+//          name : 'bharat',
+//          age : 23
+// }
+// console.log(obj.name);
+
+
+
+// var obj1 = {
+//         model : 'iphone',
+//         price : 70000,
+//         color : 'red'
+// }
+
+// var obj2 ={
+//         model : 'onepluse',
+//         price : 50000,
+//         color : 'white'
+// }
+
+// var obj3 = {
+//         model : 'samsung',
+//         price : 30000,
+//         color : 'blue'
+// }
+// console.log(obj2.model);
+
+
+
+// object ke ander function 
+
+// var user ={
+//         userName : 'bharat',
+//         age : 23,
+//         greet : function(){
+//                 console.log("Good Morning");
+                
+//         }
+// }
+// console.log(user.greet());
+
+
+// Array of objects
+var arr = [
+        {name:'bharat', age:23},
+        {name:'raju', age:25},
+        {name:'ram', age:30}
+]
+console.log(arr[1].name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
